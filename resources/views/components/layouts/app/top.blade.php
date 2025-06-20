108c24b1a4cfa0944f7cc992c407441394f58758
Tusar Gautam <131200885+Tsrgtm@users.noreply.github.com>
2025-06-20 22:19:34 +0545

    Update top.blade.php

--- resources/views/components/layouts/app/top.blade.php
+++ resources/views/components/layouts/app/top.blade.php
@@ -6,13 +6,13 @@
 </head>
 
 <body
-    class="min-h-screen bg-neutral-50 antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900 max-w-screen overflow-x-hidden">
+    class="min-h-screen bg-neutral-50 antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900 max-w-[100vw] w-full overflow-x-hidden">
     <div class="fixed top-0 w-full max-w-screen z-99">
         <x-app-top />
         <x-app-header />
     </div>
 
-    <main class="max-w-[100vw] w-full overflow-x-hidden">
+    <main>
         {{ $slot }}
     </main>
 
